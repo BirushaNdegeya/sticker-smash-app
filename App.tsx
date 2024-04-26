8826f70cbcf0d8202cc6b8235b68add0bb58ed7d
BirushaNdegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-04-26 20:25:43 +0300

    Login Page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import type { User } from './lib/defintions';
 
 const App = () => {
+   enums {
+      n, h, r, d
+   }
+   console.log()
    const {
       container
    } = styles;
+   const users: User = {
+      id: '001',
+      isActive: false,
+      description: 'something to write'
+   };
+   console.log(users);
    return (
       <View style={container}>
          <StatusBar />
